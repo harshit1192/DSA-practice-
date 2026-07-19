9372fbc181e9dd027be928415558ec987b696a2b
Harshit Singh <your_email@example.com>
2026-07-19 11:17:57 +0530

    commit

---
+++ BasicQuestions/Isprime.java
@@ -0,0 +1,25 @@
+package BasicQuestions;
+import java.util.*;
+public class Isprime {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n ==2){
+            System.out.print("number is prime");
+        }else{
+            Boolean isPrime = true;
+     for(int i=2;i<n-1;i++){
+        if(n%i==0){
+            isPrime = false;
+        }
+     }
+     if(isPrime == true){
+        System.out.print("number is prime");
+     }else{
+        System.out.print("number is not prime");
+     } 
+        }
+    
+    }
+    
+}
--- BasicQuestions/revGivenNum.java
+++ BasicQuestions/revGivenNum.java
@@ -1,16 +1,21 @@
 package BasicQuestions;
-import java.util.*;
-public class reverseNum{
+
+import java.util.Scanner;
+
+public class revGivenNum {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter your number: ");
         int n = sc.nextInt();
-
+        int rev = 0;
         while(n>0){
             int lastDigit = n%10;
+            rev = (rev*10) + lastDigit;
             n=n/10;
-            System.out.print(lastDigit);
-            sc.close();
         }
+         System.out.print(rev);
+          sc.close();
     }
+    
+    
 }
