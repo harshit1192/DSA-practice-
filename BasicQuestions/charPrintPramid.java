b8aa89cf7b706945a6cb917b4da98cdc32335baa
Harshit Singh <your_email@example.com>
2026-07-24 18:44:32 +0530

    push

---
+++ BasicQuestions/charPrintPramid.java
@@ -0,0 +1,19 @@
+package BasicQuestions;
+import java.util.*;
+public class charPrintPramid {
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        char ch = 'A';
+        for(int line=1;line<=n;line++){
+           for(int i=1; i<=line; i++){
+            System.out.print(ch);
+            ch++;
+           }
+           System.out.println();
+        }
+        
+      sc.close();
+    }
+    
+}
